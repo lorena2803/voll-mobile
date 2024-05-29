9bdf83f046a930b79a4a92a2b0416c25de6e9c9d
lorena2803 <lorenagabriela84@gmail.com>
2024-05-29 14:30:57 -0300

    Initial commit

---
+++ src/Tabs/Perfil.tsx
@@ -0,0 +1,44 @@
+import { VStack, Text, ScrollView, Avatar, Divider } from 'native-base'
+import { Titulo } from '../componentes/Titulo'
+import { useEffect, useState } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { pegarDadosPaciente } from '../servicos/PacienteServico'
+import { Paciente } from '../interfaces/Paciente'
+
+export default function Perfil() {
+  const [dadosPaciente, setDadosPaciente] = useState({} as Paciente)
+
+  useEffect(() => {
+    async function dadosPaciente() {
+      const pacienteId = await AsyncStorage.getItem('pacienteId')
+      if (!pacienteId) return null
+
+      const resultado = await pegarDadosPaciente(pacienteId)
+      if (resultado) {
+        setDadosPaciente(resultado)
+        console.log(resultado)
+      }
+    }
+    dadosPaciente()
+  })
+  return (
+    <ScrollView flex={1}>
+      <VStack flex={1} alignItems="center" p={5}>
+        <Titulo color="blue.500">Meu Perfil</Titulo>
+
+        <Avatar size="xl" source={{ uri: dadosPaciente.imagem }} mt={5} />
+
+        <Titulo color="blue.500">Informações pessoais</Titulo>
+        <Titulo fontSize="lg" mb={1}>{dadosPaciente.nome}</Titulo>
+        <Text>{dadosPaciente.email}</Text>
+        <Text>dadosPaciente.endereco.estado</Text>
+
+        <Divider mt={5} />
+
+        <Titulo color="blue.500" mb={1}>Histórico médico</Titulo>
+        <Text>Bronquite</Text>
+        <Text>Sinusite</Text>
+      </VStack>
+    </ScrollView>
+  )
+}
